9a7167aeb5f8aab304e823d847ba418c133693a9
Julekha5 <mulani.julekha93@gmail.com>
2021-12-09 11:56:08 +0530

    commit now

---
+++ src/dummy/HelloWorld.java
@@ -0,0 +1,14 @@
+package dummy;
+
+public class HelloWorld {
+
+	
+	public void display() {
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("HelloWorld");
+	}
+
+}
